1f5ad43141c5ec7bf530b514bdbf60e44b93e3ad
Ian Day <ian@ianday.me>
2024-11-17 16:52:31 -0500

    feat(Frontend): extract configure base url function

--- frontend/src/pages/auth/sign-in.tsx
+++ frontend/src/pages/auth/sign-in.tsx
@@ -5,62 +5,21 @@ import { Search } from '@/components/search'
 import useIsCollapsed from '@/hooks/use-is-collapsed'
 import ThemeSwitch from '@/components/theme-switch'
 import { UserNav } from '@/components/user-nav'
-import { IconShoppingCart } from '@tabler/icons-react'
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Button } from '@/components/custom/button'
-import {
-  Credenza,
-  CredenzaBody,
-  CredenzaClose,
-  CredenzaContent,
-  CredenzaDescription,
-  CredenzaFooter,
-  CredenzaHeader,
-  CredenzaTitle,
-  CredenzaTrigger,
-} from '@/components/credenza'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { useForm } from 'react-hook-form'
-import useLocalStorage from '@/hooks/use-local-storage'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
-import { Input } from '@/components/ui/input'
-
-const formSchema = z.object({
-  url: z.string().min(1, { message: "Please enter your backend server's URL" }),
-})
+import useLocalStorageState from '@/hooks/use-local-storage'
+import { useEffect } from 'react'
+import SetConfiguredServer from './components/set-configured-server'
 
 export default function SignIn() {
   const [isCollapsed, setIsCollapsed] = useIsCollapsed()
   const [isLoading, setIsLoading] = useState(false)
   const [showModal, setShowModal] = useState(false)
-  const [baseUrl, setBaseUrl] = useLocalStorage<string | null>({
-    key: 'base_url',
-    defaultValue: '',
-  })
+  const [baseUrl, setBaseUrl] = useLocalStorageState('base_url', '')
 
-  //setShowModal(baseUrl ? false : true)
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      url: '',
-    },
-  })
-
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    setIsLoading(true)
-    setBaseUrl(data.url)
-    console.log(data.url)
-    setIsLoading(false)
-    setShowModal(false)
-  }
+  useEffect(() => {
+    setShowModal(baseUrl ? false : true)
+  }, [baseUrl])
 
   return (
     <Layout fixed>
@@ -76,15 +35,6 @@ export default function SignIn() {
       <Layout.Body className='flex flex-col'>
         <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0'>
           <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
-            <div className='mb-4 flex items-center justify-center'>
-              <IconShoppingCart stroke={1.5} className={`mx-3`} />
-              <div
-                className={`flex flex-col justify-end truncate ${isCollapsed ? 'invisible w-0' : 'visible w-auto'}`}
-              >
-                <span className='font-medium'>Don't Forget</span>
-                {/* <span className='text-xs'>It's Important</span> */}
-              </div>
-            </div>
             <Card className='p-6'>
               <div className='flex flex-col space-y-2 text-left'>
                 <h1 className='text-2xl font-semibold tracking-tight'>Login</h1>
@@ -94,66 +44,25 @@ export default function SignIn() {
                 </p>
               </div>
               <UserAuthForm />
-              <p className='mt-4 px-8 text-center text-sm text-muted-foreground'>
+              <p className='mt-4 px-3 text-center text-sm'>
                 <Button
                   className='mt-2'
                   loading={isLoading}
                   onClick={() => setShowModal(true)}
+                  variant='secondary'
                 >
-                  {baseUrl}
+                  Configured Server: {baseUrl}
                 </Button>
               </p>
             </Card>
-            <Credenza open={showModal}>
-              <CredenzaContent>
-                <CredenzaHeader>
-                  <CredenzaTitle>Set Base URL</CredenzaTitle>
-                  <CredenzaDescription>
-                    Configure your backend's URL.
-                  </CredenzaDescription>
-                </CredenzaHeader>
-                <CredenzaBody>
-                  <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)}>
-                      <div className='grid gap-2'>
-                        <FormField
-                          control={form.control}
-                          name='url'
-                          render={({ field }) => (
-                            <FormItem className='space-y-1'>
-                              <FormLabel>URL</FormLabel>
-                              <FormControl>
-                                <Input
-                                  placeholder='https://dontforget.domain.com'
-                                  {...field}
-                                />
-                              </FormControl>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />
-                        <Button className='mt-2' loading={isLoading}>
-                          Save
-                        </Button>
-
-                        <div className='relative my-2'>
-                          <div className='absolute inset-0 flex items-center'>
-                            <span className='w-full border-t' />
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                  </Form>
-                </CredenzaBody>
-                <CredenzaFooter>
-                  <CredenzaClose asChild>
-                    <Button>Close</Button>
-                  </CredenzaClose>
-                </CredenzaFooter>
-              </CredenzaContent>
-            </Credenza>
           </div>
         </div>
+        <SetConfiguredServer
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+          showModal={showModal}
+          setShowModal={setShowModal}
+        />
       </Layout.Body>
     </Layout>
   )
--- frontend/src/pages/auth/sign-up.tsx
+++ frontend/src/pages/auth/sign-up.tsx
@@ -1,27 +1,18 @@
 import { Card } from '@/components/ui/card'
 import { SignUpForm } from './components/sign-up-form'
 import { Link } from 'react-router-dom'
-
+import SetConfiguredServer from './components/set-configured-server'
+import { useState } from 'react'
+import useLocalStorageState from '@/hooks/use-local-storage'
+import { Button } from '@/components/custom/button'
 export default function SignUp() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [baseUrl, setBaseUrl] = useLocalStorageState('base_url', '')
   return (
     <>
       <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0'>
         <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
-          <div className='mb-4 flex items-center justify-center'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='mr-2 h-6 w-6'
-            >
-              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-            </svg>
-            <h1 className='text-xl font-medium'>Shadcn Admin</h1>
-          </div>
           <Card className='p-6'>
             <div className='mb-2 flex flex-col space-y-2 text-left'>
               <h1 className='text-lg font-semibold tracking-tight'>
@@ -39,6 +30,7 @@ export default function SignUp() {
               </p>
             </div>
             <SignUpForm />
+
             <p className='mt-4 px-8 text-center text-sm text-muted-foreground'>
               By creating an account, you agree to our{' '}
               <a
@@ -56,9 +48,25 @@ export default function SignUp() {
               </a>
               .
             </p>
+            <p className='mt-4 px-3 text-center text-sm'>
+              <Button
+                className='mt-2'
+                loading={isLoading}
+                onClick={() => setShowModal(true)}
+                variant='secondary'
+              >
+                Configured Server: {baseUrl}
+              </Button>
+            </p>
           </Card>
         </div>
       </div>
+      <SetConfiguredServer
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
     </>
   )
 }
